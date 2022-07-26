1d946ba82ead43f77934eca92aed6b945663f961
cheongwonyoung <kaka491@naver.com>
2022-07-26 23:50:43 +0900

    feat : BOJ

--- class_2/src/class_2/BOJ1085.java
+++ class_2/src/class_2/BOJ1085.java
@@ -1,5 +1,20 @@
 package class_2;
 
-public class BOJ1085 {
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class BOJ1085 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String[] temp = in.readLine().split(" ");
+		int x = Integer.parseInt(temp[0]);
+		int y = Integer.parseInt(temp[1]);
+		int w = Integer.parseInt(temp[2]);
+		int h = Integer.parseInt(temp[3]);
+		int min = Math.min(Math.min(x, Math.abs(w-x)),Math.min(y, Math.abs(h-y)));
+		
+		System.out.println(min);
+	}
 }
