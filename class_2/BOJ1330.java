a219f9b923ecbabf4429685c499e4c5101c6d592
cheongwonyoung <kaka491@naver.com>
2022-07-28 00:45:25 +0900

    feat : BOJ1330

---
+++ class_2/BOJ1330.java
@@ -0,0 +1,25 @@
+package class_1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ1330 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String[] num = in.readLine().split(" ");
+		int a = Integer.parseInt(num[0]);
+		int b = Integer.parseInt(num[1]);
+		
+		if(a < b) {
+			System.out.println("<");
+		}
+		else if (a>b) {
+			System.out.println(">");
+		}
+		else {
+			System.out.println("==");
+		}
+		
+		
+	}
+}
