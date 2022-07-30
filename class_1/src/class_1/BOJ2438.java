33aa90271ef40d1a36fe1e650a4a4e60687c2228
cheongwonyoung <kaka491@naver.com>
2022-07-31 02:30:06 +0900

    feat : Add BOJ-2438-Java

---
+++ class_1/src/class_1/BOJ2438.java
@@ -0,0 +1,20 @@
+package class_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2438 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(in.readLine());
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < i+1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+}
