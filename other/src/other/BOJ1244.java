0255fe600efbfcc165fbe8d3fd596ab71cc93986
cheongwonyoung <kaka491@naver.com>
2022-08-02 14:19:20 +0900

    feat : Add-BOJ-1210-Java

--- other/src/other/BOJ1244.java
+++ other/src/other/BOJ1244.java
@@ -10,25 +10,37 @@ public class BOJ1244 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		
+		// 스위치 개수
 		int n = Integer.parseInt(in.readLine());
+		// 스위치의 상태
 		String[] switch_num = in.readLine().split(" ");
+		// 학생 수 
 		int cnt = Integer.parseInt(in.readLine());
 		
+		
 		for(int i =0; i< cnt; i++) {
+			// 학생 성별, 받은 번호
 			String temp[] = in.readLine().split(" ");
+			// 성별
 			int s = Integer.parseInt(temp[0]);
+			// 받은 번호 ( 1이상 스위치 개수 이하)
 			int num = Integer.parseInt(temp[1]);
 			
+			// 남학생
 			if(s==1) {
 				int j = 1;
 				while(num*j-1 >= 0 && num*j-1 < n) {
-					if(switch_num[num*j-1]=="1")
+
+					if(switch_num[num*j-1].equals("1")) {
 						switch_num[num*j-1] = "0";
+						}
 					else
 						switch_num[num*j-1] = "1";
 					j++;
 				}
 			}
+			
+			// 여학생
 			else if(s==2) {
 				
 				int l = 0;
@@ -53,11 +65,11 @@ public class BOJ1244 {
 		
 		for(int p =0; p<n; p++) {
 			System.out.print(switch_num[p] + " ");
-			if(p!=0 && p%19==0) {
+
+			if((p+1)%20 == 0) {
 				System.out.println();
 			}
 		}
-		
 		
 
 	}
