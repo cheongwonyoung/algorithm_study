79333a6c6e2f859ab41eb2defe18551d204a7c58
cheongwonyoung <kaka491@naver.com>
2022-08-17 17:58:04 +0900

    re

---
+++ other/src/other/BOJ1541.java
@@ -0,0 +1,15 @@
+package other;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ1541 {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+	}
+	
+}
