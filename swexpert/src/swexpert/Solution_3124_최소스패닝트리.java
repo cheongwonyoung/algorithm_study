4464d861f47ea416da0db11207f39c82e90741e2
cheongwonyoung <kaka491@naver.com>
2022-08-24 00:04:49 +0900

    feat: Add-Solution_3124-Java

--- swexpert/src/swexpert/Solution_3124_최소스패닝트리.java
+++ swexpert/src/swexpert/Solution_3124_최소스패닝트리.java
@@ -3,17 +3,34 @@ package swexpert;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.PriorityQueue;
 
 public class Solution_3124_최소스패닝트리 {
+	
+	public static class Edge implements Comparable<Edge>{
+		int a;
+		int b;
+		int c;
+		public Edge(int a, int b, int c) {
+			this.a = a;
+			this.b = b;
+			this.c = c;
+		}
+		@Override
+		public int compareTo(Edge o) {
+			return this.c - o.c;
+		}
+	}
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		
+		
 		int T = Integer.parseInt(in.readLine());
 		for(int t=1; t<=T; t++) {
-			int result = 0;
-			sb.append("#").append(t).append(" ");
+			long result = 0;
+			PriorityQueue<Edge> queue = new PriorityQueue<>();
 			String[] tem = in.readLine().split(" ");
 			int v = Integer.parseInt(tem[0]);
 			int e = Integer.parseInt(tem[1]);
@@ -28,16 +45,21 @@ public class Solution_3124_최소스패닝트리 {
 				int b = Integer.parseInt(tem[1]);
 				int c = Integer.parseInt(tem[2]);
 				
-				if(!(find(arr, a)==find(arr,b))) {
-					result += c;
-					union(arr, a, b);
+				queue.add(new Edge(a,b,c));
+			}
+			int len = queue.size();
+			for(int i=0; i<len; i++) {
+				Edge temp = queue.poll();
+				if(!(find(arr, temp.a)==find(arr,temp.b))) {
+					result += temp.c;
+					union(arr, temp.a, temp.b);
 				}
 				
-				
 			}
-			sb.append(result).append("\n");
-		}
-		System.out.println(sb);
+				
+				
+			sb.append("#").append(t).append(" ").append(result).append("\n");
+		}		System.out.println(sb);
 	
 		
 	}
@@ -54,6 +76,6 @@ public class Solution_3124_최소스패닝트리 {
 	
 	public static int find(int[] arr, int n) {
 		if(arr[n]==n) return n;
-		else return find(arr, arr[n]);
+		else return arr[n] = find(arr, arr[n]);
 	}
 }
