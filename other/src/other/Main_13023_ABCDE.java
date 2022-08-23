39fb6ba0d997de5a7717cfdacdf7d2ad9b6b5798
cheongwonyoung <kaka491@naver.com>
2022-08-23 11:39:23 +0900

    feat: BOJ-13023_ABCDE-Java

--- other/src/other/Main_13023_ABCDE.java
+++ other/src/other/Main_13023_ABCDE.java
@@ -1,5 +1,73 @@
 package other;
 
-public class Main_13023_ABCDE {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main_13023_ABCDE {
+	
+	static int n, m, cnt, result;
+
+	static List<Integer>[] list;
+	static boolean[] isCheck;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		
+		String[] temp = in.readLine().split(" ");
+		
+		n = Integer.parseInt(temp[0]); // 사람 수	
+		m = Integer.parseInt(temp[1]); // 친구관계 수
+		
+		list = new ArrayList[n];
+
+		for(int i=0; i<n; i++) {
+			list[i] = new ArrayList<>();
+		}
+			
+		
+		isCheck = new boolean[n];
+		
+		for(int i=0; i<m; i++) {
+			temp = in.readLine().split(" ");
+			int a = Integer.parseInt(temp[0]);
+			int b = Integer.parseInt(temp[1]);
+			list[a].add(b);
+			list[b].add(a);
+			
+		}
+	
+		for(int i=0; i<n; i++) {
+			cnt = 0;
+			isCheck = new boolean[n];
+			dfs(i);
+			if(result==1) break;
+		}
+
+		System.out.println(result);
+		
+		
+	}
+	
+	public static void dfs(int v) {
+		if(cnt == 4) {
+			result = 1;
+			return;
+		}
+		isCheck[v] = true;
+		for(int i =0; i<list[v].size(); i++) {
+			int tem = list[v].get(i);
+			if(!isCheck[tem]) {
+				isCheck[tem] = true;
+				cnt+=1;
+				dfs(tem);
+				isCheck[tem] = false;
+				cnt-=1;
+			}
+		}
+	}
+	
 }
--- other/src/other/Main_1697_숨바꼭질.java
+++ other/src/other/Main_1697_숨바꼭질.java
@@ -8,7 +8,6 @@ import java.util.Queue;
 
 public class Main_1697_숨바꼭질 {
 	static int k;
-<<<<<<< HEAD
 	static int result = Integer.MAX_VALUE;
 	static boolean[] arr;
 	static int cnt;
@@ -22,20 +21,10 @@ public class Main_1697_숨바꼭질 {
 //		arr = new boolean[100000];
 		bfs(n);
 
-=======
-	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		String[] temp = in.readLine().split(" ");
-		int n = Integer.parseInt(temp[0]);
-		k = Integer.parseInt(temp[1]);
-		
-		bfs(n);
->>>>>>> 16e6365bf397baeaf8bae84fbd50ef60bb9101dd
 	}
 	
 	public static void bfs(int n) {
 		Queue<Integer> queue = new ArrayDeque<Integer>();
-<<<<<<< HEAD
 		queue.offer(n);
 //		arr[n] = true;
 		
@@ -49,7 +38,7 @@ public class Main_1697_숨바꼭질 {
 			}
 //			System.out.println(temp);
 			
-//			if(!arr[temp-1]) {
+//			if(!arr[temp-1]) {	
 				queue.offer(temp-1);
 //				arr[temp-1] = true;
 //			}
@@ -79,39 +68,3 @@ public class Main_1697_숨바꼭질 {
 
 
 
-=======
-		boolean[] visit = new boolean[100001];
-		int[] check = new int[100001];
-		queue.add(n);
-		check[n] = 0;
-		visit[n] = true;
-		while(!queue.isEmpty()){
-			int tem = queue.poll();
-			
-			if(tem==k) {
-				System.out.println(check[tem]);
-				break;
-			}
-			if(tem-1 >= 0 && tem-1 <=100000 && visit[tem-1]==false)
-			{
-				queue.add(tem-1);
-				visit[tem-1] = true;
-				check[tem-1] = check[tem]+1;
-			}
-			if(tem+1 >= 0 && tem+1 <=100000 && visit[tem+1]==false)
-			{
-				queue.add(tem+1);
-				visit[tem+1] = true;
-				check[tem+1] = check[tem]+1;
-				}
-			if(tem*2 >= 0 && tem*2 <=100000 && visit[tem*2]==false)
-			{
-				queue.add(tem*2);
-				visit[tem*2] = true;
-				check[tem*2] = check[tem]+1;
-			}
-		}
-	}
-	
-}
->>>>>>> 16e6365bf397baeaf8bae84fbd50ef60bb9101dd
