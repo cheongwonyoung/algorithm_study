a5408bda5f6908c7b7b78af7c57bfe1710f46385
Cheongwonyoung <kaka491@naver.com>
2022-10-05 17:56:46 +0900

    Fe

--- other/src/other/BOJ1932.java
+++ other/src/other/BOJ1932.java
@@ -3,8 +3,7 @@ package other;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
+
 
 public class BOJ1932 {
 	
