60b97f8cad217d82b484d790f5d3b296b009b3df
Cheongwonyoung <kaka491@naver.com>
2022-10-05 14:41:13 +0900

    Feat: Add-BOJ1194-Java

--- other/src/other/BOJ1194.java
+++ other/src/other/BOJ1194.java
@@ -13,8 +13,9 @@ public class BOJ1194 {
 	static int[] dc = {0, 0, -1, 1};
 	static int n, m, move, start_i, start_j;
 	static List<int[]> end_list;
+//	static HashMap<ing, int> map = new HashMap<int, int>();
 	static List<Character> key_list;
-	static Character[][] arr;
+	static char[][] arr;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -23,11 +24,12 @@ public class BOJ1194 {
 		n = Integer.parseInt(tem[0]);
 		m = Integer.parseInt(tem[1]);
 		
-		arr = new Character[n][m];
+		arr = new char[n][m];
 		
 		start_i=0; 
 		start_j=0;
 		end_list = new ArrayList<>();
+		key_list = new ArrayList<>();
 		
 		for(int i = 0; i<n; i++) {
 			tem = in.readLine().split("");
@@ -44,55 +46,59 @@ public class BOJ1194 {
 				
 			}
 		}
-		
 		bfs(start_i, start_j);
-//		System.out.println('A'-0);
-//		System.out.println('F'-0);
+		System.out.println(move);
 	}
 
 	private static void bfs(int i, int j) {
 		ArrayDeque<int[]> queue = new ArrayDeque<>();
-		queue.add(new int[] {i, j});
-		move = -1;
+		queue.offer(new int[] {i, j});
+				
 		while(!queue.isEmpty()) {
 			int[] temp = queue.poll();
 			i = temp[0];
 			j = temp[1];
+			for(int l =0; l<end_list.size(); l++) {
+				System.out.println("endlist");
+				System.out.println(end_list.get(l)[0]);
+				System.out.println(end_list.get(l)[1]);
+			}
 			
+			
+//			System.out.println("?");
 			for(int d = 0; d <4; d++) {
 				int nr = i + dr[d];
 				int nc = j + dc[d];
 				
 				// 예외 범위 또는 벽(이동 x)
-				if(nr<0 || nc<0 || nr>=n || nc>=m || arr[nr][nc].equals('#')) continue;
+				if(nr<0 || nc<0 || nr>=n || nc>=m || arr[nr][nc]=='#') continue;
 				
 				// 문인데 키가 없으면(이동 x)
 				else if((arr[nr][nc]-0 >=65 && arr[nr][nc]-0<=70) && !key_list.contains(arr[nr][nc])) 
 					continue;
 				
 				// 키 추가 (이동 o)
-				else if(arr[nr][nc].equals('a')) {
+				else if(!key_list.contains('A') && arr[nr][nc]=='a') {
 					arr[nr][nc] = '.';
 					key_list.add('A');
-				}else if(arr[nr][nc].equals('b')) {
+				}else if(!key_list.contains('B') &&arr[nr][nc]=='b') {
 					arr[nr][nc] = '.';
 					key_list.add('B');
-				}else if(arr[nr][nc].equals('c')) {
+				}else if(!key_list.contains('C') &&arr[nr][nc]=='c') {
 					arr[nr][nc] = '.';
 					key_list.add('C');
-				}else if(arr[nr][nc].equals('d')) {
+				}else if(!key_list.contains('D') &&arr[nr][nc]=='d') {
 					arr[nr][nc] = '.';
 					key_list.add('D');
-				}else if(arr[nr][nc].equals('e')) {
+				}else if(!key_list.contains('E') &&arr[nr][nc]=='e') {
 					arr[nr][nc] = '.';
 					key_list.add('E');
-				}else if(arr[nr][nc].equals('f')) {
+				}else if(!key_list.contains('F') &&arr[nr][nc]=='f') {
 					arr[nr][nc] = '.';
 					key_list.add('F');
 				}
 				
 				queue.offer(new int[] {nr,nc});
-				if()
 				move += 1;
 				
 			}
