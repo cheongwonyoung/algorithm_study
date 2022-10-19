dfd2caed6cbd228072add366cb49bdbd4df86312
cheongwonyoung <kaka491@naver.com>
2022-10-20 02:01:01 +0900

    Feat: Add-BOJ1389-케빈베이컨6단계법칙

--- other/src/other/BOJ1389.java
+++ other/src/other/BOJ1389.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class BOJ9935 {
+public class BOJ1389 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
